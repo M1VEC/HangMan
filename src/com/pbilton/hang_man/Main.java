51733d01888082e087b804d47aa8de3743dfc300
Philip Bilton <pbilton85@gmail.com>
2020-10-03 19:52:38 +0100

    Inital Commit

---
+++ src/com/pbilton/hang_man/Main.java
@@ -0,0 +1,8 @@
+package com.pbilton.hang_man;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/pbilton/test/com/pbilton/hang_man/testHangMan.java
@@ -0,0 +1,11 @@
+package com.pbilton.hang_man;
+
+import org.testng.annotations.Test;
+
+public class testHangMan {
+
+    @Test
+    public void test(){
+        
+    }
+}
